a46d4788b1c8c92060222207756125f3db14826c
meeb <meeb@meeb.org>
2015-05-04 04:13:12 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # django-cachebuster
-CDN cache busting statis file URL modifier for Django
+
+`django-cachebuster` is a small add-on for Django that adds the `cdnstatic`
+template tag which adds a 'cache busting' dynamic query string appended to the
+file paths. This tag functions identically to the standard `{% static ... %}`
+template tag. This is designed to work with CDNs that cache by complete URI
+including the query string so when you push a change the CDNs are automatically
+refreshed without having to wait for TTLs to expire. Internally we use this with
+`django-distill`, a static site generator for Django to work with static sites
+with heavy caching on images behind CloudFlare, cachefly and other CDNs:
+
+https://github.com/mgrp/django-distill
+
+Under the hood, this module just chains the request to the existing `static` tag
+to be widely compatible.
+
+# Installation
+
+Install from pip:
+
+```bash
+$ pip install --upgrade git+git://github.com/mgrp/django-cachebuster.git@master
+```
+
+Add `django_cachebuster` to your `INSTALLED_APPS` in your `settings.py`:
+
+```python
+INSTALLED_APPS += ('django_cachebuster',)
+```
+
+That's it. Once installed, all calls to the `static` template tag will be
+intercepted and rewritten for you.
+
+# Usage
+
+Load the new module at the top of your templates:
+
+```html
+{% load cdnstaticfiles %}
+```
+
+Then use it in the template exactly as you would use the `static`:
+
+```html
+{% cdnstatic 'some/image.jpg' %}
+```
+This renders as (assuming `settings.STATIC_URL` is set to `/static/`):
+
+```html
+/static/some/image.jpg?tag=[random tag]
+```
+
+`[random tag]` is either the truncated mercurial or git commit reference if it
+is available, otherwise it will be the current date in `YYYYMMDDHHMMSS` format.
+If there are existing query string parameters then the `tag` is intelligently
+appended. The `cdnstatic` tag will not break the existing URL.
+
+# Contributing
+
+All properly formatted and sensible pull requests, issues and comments are
+welcome.
---
+++ django_cachebuster/templatetags/cdnstaticfiles.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+from django.template import (Library, Node)
+from django.utils.timezone import now
+from django.utils.six import string_types
+from django.utils.six.moves.urllib.parse import (urlsplit, urlunsplit)
+from django.templatetags.static import do_static
+
+register = Library()
+
+class RepoRefCache(object):
+    '''
+        Simple reference state cache so the reference tags are only requested
+        once.
+    '''
+
+    def __init__(self):
+        self._git_tip = None
+        self._git_failed = False
+        self._hg_tip = None
+        self._hg_failed = False
+
+    def _run_cmd(self, cmd):
+        try:
+            return subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError:
+            return None
+
+    def _strip_ref(self, ref):
+        first_word = ref.split(' ')[0]
+        return first_word.replace('"', '').replace('\'', '').strip()
+
+    def get_git_tip(self):
+        if self._git_failed:
+            return None
+        elif self._git_tip:
+            return self._git_tip[:14]
+        ref = self._run_cmd(['git', 'rev-parse', 'HEAD'])
+        if isinstance(ref, string_types):
+            self._git_tip = self._strip_ref(ref)
+            return self._git_tip[:14]
+        else:
+            self._git_failed = False
+            return None
+
+    def get_hg_tip(self):
+        if self._hg_failed:
+            return None
+        elif self._hg_tip:
+            return self._hg_tip[:14]
+        ref = self._run_cmd(['hg', 'heads', '--template', '"{node}"'])
+        if isinstance(ref, string_types):
+            self._hg_tip = self._strip_ref(ref)
+            return self._hg_tip[:14]
+        else:
+            self._hg_failed = True
+            return None
+
+    def get_ts(self):
+        return now().strftime('%Y%m%d%H%M%S')
+
+    def get_ref(self):
+        ref = self.get_git_tip()
+        if ref:
+            return ref
+        ref = self.get_hg_tip()
+        if ref:
+            return ref
+        ref = self.get_ts()
+        if ref:
+            return ref
+        return None
+
+refcache = RepoRefCache()
+
+class CDNStaticURLNode(Node):
+    def __init__(self, url):
+        self.url = url
+    def render(self, context):
+        url = self.url.render(context)
+        parts = urlsplit(url)
+        qs = parts.query + '&tag=' + refcache.get_ref()
+        return urlunsplit((parts.scheme, parts.netloc, parts.path, qs,
+            parts.fragment))
+
+@register.tag('cdnstatic')
+def do_cdnstatic(parser, token):
+    url = do_static(parser, token)
+    return CDNStaticURLNode(url)
+
+# eof
